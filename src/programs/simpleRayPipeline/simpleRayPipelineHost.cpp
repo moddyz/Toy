79a880c64067fce07ca6c86855bc383569821601
moddyz <moddy3d@gmail.com>
2020-09-13 19:16:39 +1200

    Setup simpleRayPipeline program.

---
+++ src/programs/simpleRayPipeline/simpleRayPipelineHost.cpp
@@ -0,0 +1,9 @@
+#include "simpleRayPipelineWindow.h"
+
+int main( int i_argc, char** i_argv )
+{
+    TOY_LOG_INFO( "[Starting simpleRayPipeline...]\n" );
+    toy::SimpleRayPipelineWindow< toy::Host > window( "Toy: simpleRayPipeline", gm::Vec2i( 640, 480 ) );
+    window.Run();
+    return 0;
+}
