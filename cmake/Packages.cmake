3b2f59e8dd29a39df8ff2ea5f5684015d5dd58fa
moddyz <moddy3d@gmail.com>
2020-10-11 22:27:27 +1300

    GLEW_LIBRARIES workaround.

--- cmake/Packages.cmake
+++ cmake/Packages.cmake
@@ -1,6 +1,12 @@
 # Discover project dependencies.
 
 find_package(GLFW REQUIRED)
+
+# GLEW Workaround.
 find_package(GLEW REQUIRED)
+if(NOT GLEW_LIBRARIES)
+    set(GLEW_LIBRARIES GLEW::GLEW)
+endif()
+
 find_package(OpenGL REQUIRED)
 find_package(CUDAToolkit REQUIRED 10.0)
