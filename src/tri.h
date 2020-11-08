1a8a12415116c56c2e9591064ef33b777bb4cc23
moddyz <moddy3d@gmail.com>
2020-11-08 14:24:30 +1300

    Some more code sketching

--- src/tri.h
+++ src/tri.h
@@ -4,15 +4,14 @@
 ///
 /// Tri API
 ///
-/// \b Tri is a software-based rasterization-based renderer.
+/// \b Tri is a software renderer implementing a fixed rasterization pipeline.
 ///
-/// This header provides all the API entry points of for producing graphics with
-/// the Tri renderer.
+/// This header provides all the API entry points of the Tri renderer.
 
-/// \enum TriDevice
+/// \enum TriRuntimeTarget
 ///
-/// The device used to execute the graphics commands.
-enum class TriDevice : char
+/// The runtime target used to execute the graphics operations.
+enum class TriRuntimeTarget : char
 {
     CPU = 0,
     CUDA,
@@ -21,46 +20,95 @@ enum class TriDevice : char
 
 /// \class TriContext
 ///
-/// Storage of top-level state and represents an instance of a graphics
-/// processing system.
+/// Describes root-level properties of a Tri renderer.
+///
+/// After the context is initialized with specified properties, those
+/// properties are \em immutable for the lifetime of the context object.
+///
+/// The client must construct a new context if different property
+/// specifications are desired.
+///
+/// Keep a TriContext object alive for the duration produced child objects.
+/// Any class which takes a TriContext as a constructor argument is
+/// considered a child object of the associated TriContext object.
 ///
 /// Example usage:
 /// \code{.cpp}
-/// // Expected behavior when running on a machine with CUDA support.
+/// // Expected behavior when running on a machine with CUDA runtime support.
 ///
 /// // Preferred context.
 /// TriContext preferredCtx;
-/// assert(preferredCtx.GetDevice() == TriDevice::CUDA);
+/// assert(preferredCtx.GetRuntimeTarget() == TriRuntimeTarget::CUDA);
 ///
-/// // Construct a context with CPU device selection.
-/// TriContext cpuCtx(TriDevice::CPU);
-/// assert(cudaCtx.GetDevice() == TriDevice::CPU);
+/// // Construct a context with CPU runtime target selection.
+/// TriContext cpuCtx(TriRuntimeTarget::CPU);
+/// assert(cudaCtx.GetRuntimeTarget() == TriRuntimeTarget::CPU);
 /// \endcode
 class TriContext : final
 {
 public:
-    /// Construct a TriContext with the preferred device.
+    /// Construct a TriContext object with the preferred runtime target.
     ///
-    /// The "preferred" device is selected based on priority and availability,
-    /// by starting from the largest value of \ref TriDevice (bar Count)
-    /// and incrementing backwards.
+    /// The "preferred" runtime target is selected based on priority and
+    /// availability, by starting from the largest value of \ref
+    /// TriRuntimeTarget (bar TriRuntimeTarget::Count) and incrementing backwards.
     TriContext();
 
-    /// Construct a TriContext for a requested device.
+    /// Construct a TriContext for a requested runtime target.
     ///
-    /// \note The requested device may not be available, in which the fallback
-    /// CPU device will be selected.
+    /// \note The requested runtime target may not be available, in which the
+    /// fallback CPU runtime target will be selected.
     ///
-    /// \param i_device The device used to execute graphics commands.
-    explicit TriContext(TriDevice i_requestedDevice);
+    /// \param i_runtime target The runtime target used to execute graphics
+    /// commands.
+    explicit TriContext(TriRuntimeTarget i_requestedRuntimeTarget);
 
-    /// Get the actual device selected by this context.
+    /// Get the actual runtime target selected by this context.
     ///
-    /// \return The device used to execute graphics commands.
-    inline TriDevice GetDevice() const { return m_device; }
+    /// \return The runtime target used to execute graphics commands.
+    inline TriRuntimeTarget GetRuntimeTarget() const { return m_runtimeTarget; }
 
 private:
-    TriDevice m_device;
+    TriRuntimeTarget m_runtimeTarget;
+};
+
+/// \class TriProgram
+///
+/// An executable program
+class TriProgram : final
+{
+public:
+    /// Construct a program which executes code for a given TriContext.
+    ///
+    /// \param i_context The context object providing root-level specification.
+    TriProgram(const TriContext& i_context);
+
+private:
+    const TriContext& m_context;
+};
+
+/// \class TriIndexedTriangleList
+///
+/// The geometry specification of an object to draw in the scene.
+class TriIndexedTriangleList : final
+{};
+
+/// \class TriDrawCommand
+///
+/// Combination of geometry, transformation parameters, and shader programs
+/// which are compose a single draw invocation.
+class TriDrawCall : final
+{
+public:
+    /// Set an executable program to be run on each input vertex before primitive assembly.
+    ///
+    /// \param i_program The vertex program.
+    SetVertexProgram(const TriProgram* i_program);
+    SetFragmentProgram(const TriProgram* i_program);
+
+private:
+    const TriProgram* m_vertexProgram = nullptr;
+    const TriProgram* m_fragmentProgram = nullptr;
 };
 
 /// \class TriDrawParams
@@ -69,13 +117,10 @@ private:
 class TriDrawParams : final
 {
 public:
-    /// Set the transformation matrix which moves an object from/ camera-space
+    /// Set the transformation matrix which moves an object from camera-space
     /// into world-space.
     ///
     /// Also known as the 'camera' transform.
-    ///
-    /// \note There is a computation cost in setting this transform - as the
-    /// matrix will need to be \em inverted before it is successfully stored.
     ///
     /// \param i_cameraTransform The camera-to-world transformation matrix.
     ///
@@ -83,8 +128,8 @@ public:
     /// invertable).
     bool SetCameraTransform(const gm::Mat4f& i_cameraTransform);
 
-    /// Set the transformation matrix which moves objects from camera-space
-    /// into the clipping space.
+    /// Set the transformation matrix which moves objects from camera space
+    /// into the view-frustum or clipping space.
     ///
     /// Also known as the 'projection' transformation.
     ///
