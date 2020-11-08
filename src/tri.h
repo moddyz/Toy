ff3b96945adf417e57e9eb756e1fdf5e8e234e69
moddyz <moddy3d@gmail.com>
2020-11-08 13:01:53 +1300

    Documented TriContext.

--- src/tri.h
+++ src/tri.h
@@ -4,8 +4,10 @@
 ///
 /// Tri API
 ///
-/// This header provides all the API entry points of to use the Tri
-/// software graphics pipeline.
+/// \b Tri is a software-based rasterization-based renderer.
+///
+/// This header provides all the API entry points of for producing graphics with
+/// the Tri renderer.
 
 /// \enum TriDevice
 ///
@@ -13,51 +15,93 @@
 enum class TriDevice : char
 {
     CPU = 0,
-    CUDA
+    CUDA,
+    Count
 };
 
 /// \class TriContext
 ///
 /// Storage of top-level state and represents an instance of a graphics
 /// processing system.
-class TriContext
+///
+/// Example usage:
+/// \code{.cpp}
+/// // Expected behavior when running on a machine with CUDA support.
+///
+/// // Preferred context.
+/// TriContext preferredCtx;
+/// assert(preferredCtx.GetDevice() == TriDevice::CUDA);
+///
+/// // Construct a context with CPU device selection.
+/// TriContext cpuCtx(TriDevice::CPU);
+/// assert(cudaCtx.GetDevice() == TriDevice::CPU);
+/// \endcode
+class TriContext : final
 {
-    /// Constructor with default options.
-    TriContext( TriDevic );
+public:
+    /// Construct a TriContext with the preferred device.
+    ///
+    /// The "preferred" device is selected based on priority and availability,
+    /// by starting from the largest value of \ref TriDevice (bar Count)
+    /// and incrementing backwards.
+    TriContext();
+
+    /// Construct a TriContext for a requested device.
+    ///
+    /// \note The requested device may not be available, in which the fallback
+    /// CPU device will be selected.
+    ///
+    /// \param i_device The device used to execute graphics commands.
+    explicit TriContext(TriDevice i_requestedDevice);
+
+    /// Get the actual device selected by this context.
+    ///
+    /// \return The device used to execute graphics commands.
+    inline TriDevice GetDevice() const { return m_device; }
 
 private:
+    TriDevice m_device;
 };
 
 /// \class TriDrawParams
 ///
 /// Root-level parameters for drawing.
-class TriDrawParams
+class TriDrawParams : final
 {
 public:
-    /// Set the transformation matrix which moves an object from
-    /// camera-space into world-space.
+    /// Set the transformation matrix which moves an object from/ camera-space
+    /// into world-space.
     ///
     /// Also known as the 'camera' transform.
     ///
-    /// \param i_cameraToWorld The camera-to-world transformation matrix.
-    void SetCameraTransform( const gm::Mat4f& i_cameraToWorld );
+    /// \note There is a computation cost in setting this transform - as the
+    /// matrix will need to be \em inverted before it is successfully stored.
+    ///
+    /// \param i_cameraTransform The camera-to-world transformation matrix.
+    ///
+    /// \return If the transformation was successfully set (the matrix is
+    /// invertable).
+    bool SetCameraTransform(const gm::Mat4f& i_cameraTransform);
 
     /// Set the transformation matrix which moves objects from camera-space
-    /// into the clipping space
+    /// into the clipping space.
     ///
     /// Also known as the 'projection' transformation.
     ///
-    /// \param i_cameraToWorld The camera-to-clip transformation matrix.
-    void SetProjectionTransform( const gm::Mat4f& i_cameraToClip );
+    /// \param i_projectionTransform The camera-to-clip transformation matrix.
+    void SetProjectionTransform(const gm::Mat4f& i_projectionTransform);
 
-    /// Set the transform which moves objects from clip space into viewport space.
+    /// Set the transform which moves objects from clip space into viewport
+    /// space.
     ///
     /// Also known as the "viewport" transformation.
     ///
-    /// \param i_clipToViewport The camera-to-clip transformation matrix.
-    void SetViewportTransform( const gm::Mat4f& i_clipToViewport );
+    /// \param i_viewportTransform The camera-to-clip transformation matrix.
+    void SetViewportTransform(const gm::Mat4f& i_viewportTransform);
 
 private:
-    gm::Mat4f m_worldToCamera;
+    gm::Mat4f m_cameraTransform;
+    gm::Mat4f m_projectionTransform;
+    gm::Mat4f m_viewportTransform;
 };
 
